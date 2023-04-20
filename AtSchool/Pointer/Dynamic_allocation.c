54119a6b3c9a06abcda2e5ca9db3ba9bddae9f4c
_ncduy_ <ncduy1906@gmail.com>
2023-04-21 06:15:06 +0700

    modified:   AtSchool/Pointer/Dynamic_allocation.c 	modified:   AtSchool/Pointer/Dynamic_allocation.exe

--- AtSchool/Pointer/Dynamic_allocation.c
+++ AtSchool/Pointer/Dynamic_allocation.c
@@ -1,12 +1,21 @@
 /*
 int a[1000] -> cấp phát tĩnh 1000 số int (1 int -> 4 byte)
 cấp phát động: cần đến đâu cấp phát đến đó
-VD: đọc input n = 10 -> chỉ xin OS cấp phảt array 10 phần tử vừa đủ lưu trữ dãy đầu vào n phần tử
+VD: đọc input n = 10 -> chỉ xin OS cấp phát array 10 phần tử vừa đủ lưu trữ dãy đầu vào n phần tử
 */
 
 #include <stdio.h>
+#include <stdlib.h>
 int main(){
     int i, n, *p;
     printf("How many numbers do you want to enter?\n");
     scanf("%d", &n);
+    p = (int *)malloc(n * sizeof(int));
+    if(p == NULL){
+        printf("Memory allocation failed\n");
+        return 1;
+    }
+
+    free(p); // deallocate
+    return 0;
 }
