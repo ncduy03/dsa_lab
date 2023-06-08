3bf2d9729822fbe794cd5912bd41adb00a7d6387
_ncduy_ <ncduy1906@gmail.com>
2023-06-08 09:47:59 +0700

    modified:   AtSchool/Linked_lists/Linked_list.c 	new file:   AtSchool/Linked_lists/Linked_list.exe 	new file:   AtSchool/Linked_lists/Profile_management.c 	new file:   AtSchool/Linked_lists/Profile_management.exe 	new file:   OpenERP/EXERCISES/Chapter3/Linked_list_manipulation.c 	new file:   OpenERP/EXERCISES/Chapter3/Linked_list_manipulation.exe 	deleted:    Singly_linked_list.c 	deleted:    Singly_linked_list.exe

--- AtSchool/Linked_lists/Linked_list.c
+++ AtSchool/Linked_lists/Linked_list.c
@@ -5,6 +5,8 @@ typedef struct Node{
     struct Node* next; // point to the next element of the current element
 }Node;
 
+Node* head = NULL;
+
 Node* makeNode(int v){
     Node* p = (Node*)malloc(sizeof(Node));
     p->value = v; 
@@ -18,5 +20,124 @@ Node* insertLast(Node* h, int v){
         return makeNode(v);
     }
     // general case
-    
+    while(p->next != NULL){
+        p = p->next;
+    }
+    Node* q = makeNode(v);
+    p->next = q;
+    return h;
+}
+
+Node* insertLastRecursive(Node* h, int v){
+    if(h == NULL){
+        return makeNode(v);
+    }
+    h->next = insertLastRecursive(h->next, v);
+    return h;
+}
+
+Node* removeNode(Node* h, int v){
+    Node* p = h;
+    if(h == NULL) return NULL;
+    if(h->value == v){
+        Node* tmp = h; 
+        h = h->next;      
+        free(tmp);  return h;
+    }
+    while(p->next != NULL){
+        if(p->next->value == v) break;
+        p = p->next;
+    }
+    if(p->next != NULL){
+        Node* q = p->next; 
+        p->next = q->next; 
+        free(q);
+    }
+    return h;
+}
+
+Node* removeNodeRecursive(Node* h, int v){
+    if(h == NULL) return NULL;
+    if(h->value == v){
+        Node* tmp = h;
+        h = h->next; 
+        free(tmp); 
+        return h;
+    }
+    h->next = removeNodeRecursive(h->next, v);
+    return h;
+}
+
+Node* removeAll(Node* h, int v){
+    // remove all nodes having value v from the linked list headed by h
+    if(h == NULL) return NULL;
+    if(h->value == v){
+        Node* tmp = h; h = h->next; free(tmp);
+        h = removeAll(h,v); // continue to remove other elements having value v
+        return h;
+    }
+    h->next = removeAll(h->next,v);
+    return h;
+}
+
+int count(Node* h){
+    int cnt = 0;
+    Node* p = h;
+    while(p != NULL){
+        cnt += 1;
+        p = p->next;
+    }
+    return cnt;
+}
+
+int countRecursive(Node* h){
+    if(h == NULL) return 0;
+    return 1+countRecursive(h->next);
+}
+
+Node* reverse(Node *h){
+    Node* p = h;
+    Node* pp = NULL;
+    Node* np = NULL;
+    while(p != NULL){
+        np = p->next;
+        p->next = pp;
+        pp = p;
+        p = np;
+    }
+    return pp;
+}
+
+int main(){
+    head = insertLastRecursive(head, 1);
+    head = insertLast(head, 2);
+    head = insertLast(head, 3);
+
+    printf("List: \n");
+    Node* current = head;
+    while (current != NULL) {
+        printf("%d ", current->value);
+        current = current->next;
+    }
+    printf("\n");
+
+    int nodeCount = count(head);
+    printf("%d\n", nodeCount);
+
+    head = reverse(head);
+    Node* p = head;
+    while (p != NULL) {
+        printf("%d ", p->value);
+        p = p->next;
+    }
+    printf("\n");
+
+    // Giải phóng bộ nhớ đã cấp phát cho danh sách
+    Node* temp;
+    while (head != NULL) {
+        temp = head;
+        head = head->next;
+        free(temp);
+    }
+    return 0;
 }
---
+++ OpenERP/EXERCISES/Chapter3/Linked_list_manipulation.c
@@ -0,0 +1,140 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+typedef struct Node{
+    int value;
+    struct Node* next;
+}Node;
+
+Node* head = NULL;
+
+Node* makeNode(int v){
+    Node* p = (Node*)malloc(sizeof(Node));
+    p->value = v; 
+    p->next = NULL;
+    return p;
+}
+
+Node* insertLast(Node* h, int v){
+    if(h == NULL){
+        return makeNode(v);
+    }
+    h->next = insertLast(h->next, v);
+    return h;
+}
+
+Node* insertFirst(Node* h, int v) {
+    Node* p = makeNode(v);
+    p->next = h;
+    return p;
+}
+
+Node* reverse(Node *h){
+    Node* p = h;
+    Node* pp = NULL;
+    Node* np = NULL;
+    while(p != NULL){
+        np = p->next;
+        p->next = pp;
+        pp = p;
+        p = np;
+    }
+    return pp;
+}
+
+void printList(Node* h){
+    Node* p = h;
+    while (p != NULL) {
+        printf("%d ", p->value);
+        p = p->next;
+    }
+    printf("\n");
+}
+
+Node* addAfter(Node* h, int u, int v){
+    Node* p = h;
+    while (p != NULL) {
+        if (p->value == v){
+            Node* q = makeNode(u);
+            q->next = p->next;
+            p->next = q;
+            break;
+        }
+        p = p->next;
+    }
+    return h;
+}
+
+Node* addBefore(Node* h, int u, int v){
+    Node* p = h;
+    Node* prev = NULL;
+    while (p != NULL) {
+        if (p->value == v){
+            Node* q = makeNode(u);
+            if (prev == NULL){
+                q->next = p;
+                h = q;
+            }else{
+                q->next = p;
+                prev->next = q;
+            }
+            break;
+        }
+        prev = p;
+        p = p->next;
+    }
+    return h;
+}
+
+Node* removeNode(Node* h, int v){
+    if(h == NULL) return NULL;
+    if(h->value == v){
+        Node* tmp = h;
+        h = h->next; 
+        free(tmp); 
+        return h;
+    }
+    h->next = removeNode(h->next, v);
+    return h;
+}
+
+int main(){
+    int n; scanf("%d", &n);
+    for(int i = 0; i < n; i++){
+        int x; scanf("%d", &x);
+        head = insertLast(head, x);
+    }
+    while(1){
+        char cmd[256];
+        scanf("%s",cmd);
+        if(strcmp(cmd, "#") == 0){
+            printList(head);
+            break;
+        }
+        else if(strcmp(cmd, "addlast") == 0){
+            int x; scanf("%d", &x);
+            if(findNode(x) == 0)
+                insertLast(head, x);
+        } 
+        else if(strcmp(cmd, "addfirst") == 0){
+            int x; scanf("%d", &x);
+            if(findNode(x) == 0)
+                head = insertFirst(head, x);
+        }
+        else if(strcmp(cmd, "addafter") == 0){
+            int u, v; scanf("%d %d", &u, &v);
+            head = addAfter(head, u, v);
+        }
+        else if(strcmp(cmd, "addbefore") == 0){
+            int u, v; scanf("%d %d", &u, &v);
+            head = addBefore(head, u, v);
+        }
+        else if(strcmp(cmd, "remove") == 0){
+            int x; scanf("%d", &x);
+            removeNode(head, x);
+        }
+        else if(strcmp(cmd, "reverse") == 0){
+            head = reverse(head);
+        }
+    }
+}
