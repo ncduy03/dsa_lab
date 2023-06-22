6ca0444d20dada6ba54a8ae19a962caccad8ad5d
_ncduy_ <ncduy1906@gmail.com>
2023-06-22 18:12:26 +0700

    new file:   AtSchool/Trees/Tree_height_and_depth.c 	new file:   AtSchool/Trees/Tree_height_and_depth.exe 	new file:   OpenERP/TEST/Midterm2/Count_odd_even_elements_from_seq.c 	new file:   OpenERP/TEST/Midterm2/Count_odd_even_elements_from_seq.exe 	new file:   OpenERP/TEST/Midterm2/Sudoku_check.c

---
+++ OpenERP/TEST/Midterm2/Sudoku_check.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int t;
+int a[10][10];
+void input(){
+    for(int i = 0; i < 9; i++){
+        for(int j = 0; j < 9; j++){
+            scanf("%d", &a[i][j]);
+        }
+    }
+}
+
+int check(int a[10][10]){
+    
+}
+int main(){
+    scanf("%d", &t);
+    while(t--){
+        input();
+
+    }
+}
